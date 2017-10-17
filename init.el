1954db0d39b9bf480928fa86f7f63853711ae884
Sriduth Jayhari <sriduth.jayhari@juspay.in>
2017-10-17 11:13:39 +0530

    Add latest init.el

--- init.el
+++ init.el
@@ -113,12 +113,12 @@ by Prelude.")
 (require 'prelude-global-keybindings)
 
 
-(load-theme 'zonokai-blue)
+;;(load-theme 'zonokai-blue)
 ;; Do not load anything other than the prelude packages and global keybindings
 
 ; (require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
 ; (require 'prelude-ui)
-; (require 'prelude-core)
+(require 'prelude-core)
 ; (require 'prelude-mode)
 ; (require 'prelude-editor)
 ; 
@@ -154,7 +154,7 @@ by Prelude.")
 
 ;; Utility function to open default org tasks file
 (defun open-org-tasks () (find-file "~/org/Tasks.org"))
-(global-set-key (kbd "M-q") (open-org-tasks))
+(global-set-key (kbd "M-q") (lambda () (interactive) (open-org-tasks)))
 
 ;; Sublime like keybinding for opening list of file
 (global-set-key (kbd "M-p") 'projectile-find-file) 
@@ -229,11 +229,34 @@ by Prelude.")
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook 'show-paren-mode)
 (add-hook 'prog-mode-hook 'rainbow-mode)
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'prog-mode-hook 'diff-hl-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
 
-;; Prevent the system from asking to load the theme everytime.
-(setq sml/no-confirm-load-theme t)
-(sml/setup)
-(setq sml/theme 'powerline)
+;; Enable beacon-mode globally
+(beacon-mode)
+
+(setq beacon-blink-delay 0.5)
+(setq beacon-blink-when-window-changes t)
+
+;; Enable EditorConfig mode globally
+(editorconfig-mode)
+
+(add-hook 'web-mode-hook
+	  (lambda ()
+	    (if (equal web-mode-content-type "javascript")
+		(web-mode-set-content-type "jsx"))))
+(setq js2-basic-offset 2)
+
+;; Setup company-mode for autocomplete
+(global-company-mode)
+
+
+;; Setup diff-hl mode
+(diff-hl-mode)
+
+(global-flycheck-mode)
+
 ;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -242,10 +265,41 @@ by Prelude.")
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("66132890ee1f884b4f8e901f0c61c5ed078809626a547dbefbb201f900d03fd8" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3632cf223c62cb7da121be0ed641a2243f7ec0130178722554e613c9ab3131de" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "21443b9ce8ea8c14f84a625fe70a5be257849ca60348b40da8a2cc408162c7aa" "4575b3521ce30b132011274678b32c8d8406eb1bac6856f7d067dd1bd70e521c" "f04122bbc305a202967fa1838e20ff741455307c2ae80a26035fbf5d637e325f" "66132890ee1f884b4f8e901f0c61c5ed078809626a547dbefbb201f900d03fd8" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e")))
+ '(package-selected-packages
+   (quote
+    (molokai-theme erlang csv-mode markdown-mode web-mode flycheck-purescript psc-ide groovy-mode dumb-jump company zop-to-char zonokai-theme zenburn-theme yaml-mode which-key volatile-highlights vkill undo-tree swiper solarized-theme smartrep smartparens smart-mode-line-powerline-theme rainbow-mode rainbow-delimiters ov operate-on-number neotree move-text magit json-mode js2-mode jinja2-mode imenu-anywhere helm-projectile haskell-mode guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elixir-mode editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu all-the-icons ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :family "SF Mono")))))
+ '(default ((t (:height 130 :family "SF Mono"))))
+ '(neo-dir-link-face ((t (:foreground "deep sky blue" :slant normal :weight bold :height 120 :family "SF Mono"))))
+ '(neo-file-link-face ((t (:foreground "White" :weight normal :height 120 :family "SF Mono")))))
+(load-theme 'zonokai-blue)
+
+;; Prevent the system from asking to load the theme everytime.
+(setq sml/no-confirm-load-theme t)
+(sml/setup)
+(setq sml/theme 'powerline)
+
+(require 'psc-ide)
+
+(setq psc-ide-use-npm-bin t)
+
+(add-hook 'purescript-mode-hook
+  (lambda ()
+    (psc-ide-mode)
+    (company-mode)
+    (flycheck-mode)
+    (turn-on-purescript-indentation)))
+
+(set-face-background 'vertical-border "black")
+(set-face-foreground 'vertical-border (face-background 'vertical-border))
+(setq linum-format " %d")
+(set-face-attribute 'fringe nil :background nil)
+
+;; (global-set-key (kbd "M-TAB") 'company-complete)
+
+
